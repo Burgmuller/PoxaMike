8e08a33e72a7b2dad44a1587e54698584099f2fa
Burgmuller <MikaelBurgmuller@gmail.com>
2020-02-02 00:38:33 -0300

    Update BannerPoxaMike.js

--- BannerPoxaMike.js
+++ BannerPoxaMike.js
@@ -1,3 +1,3 @@
-<button class="btnDownload" onclick="window.open('http://onvictinitor.com/afu.php?zoneid=3048678')" style="border-radius: 4px; border: none; box-shadow: -3px 4px 10px rgba(0 , 0 , 0 , 0.2); color: white; cursor: pointer; font-family: inherit; font-size: 17px; font-weight: bold; height: 50px; margin-top: 10px; outline: none; width: 256px;">👉Clique Aqui e copie o link👈</button></center>
-<div style="font-family: &quot;times new roman&quot;; font-size: medium;">
-<style>.btnDownload{background-color:#34ba44;}.btnDownload:hover{background-color:#52d762;transition:All .7s ease;}</style>
+&lt;button class=&quot;btnDownload&quot; onclick=&quot;window.open(&#39;http://onvictinitor.com/afu.php?zoneid=3048678&#39;)&quot; style=&quot;border-radius: 4px; border: none; box-shadow: -3px 4px 10px rgba(0 , 0 , 0 , 0.2); color: white; cursor: pointer; font-family: inherit; font-size: 17px; font-weight: bold; height: 50px; margin-top: 10px; outline: none; width: 256px;&quot;&gt;👉Clique Aqui e copie o link👈&lt;/button&gt;&lt;/center&gt;
+&lt;div style=&quot;font-family: &amp;quot;times new roman&amp;quot;; font-size: medium;&quot;&gt;
+&lt;style&gt;.btnDownload{background-color:#34ba44;}.btnDownload:hover{background-color:#52d762;transition:All .7s ease;}&lt;/style&gt;
